fda53dab6bcdbf9710582c83d5e4607e04856b4e
Sonikesh <91758635+SonikeshKumar14@users.noreply.github.com>
2023-09-05 23:56:32 +0530

    Add files via upload

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import "../styles/index.scss";
+
+const Footer: React.FC = () => {
+
+  return (
+    <footer>
+      <span>© 2023 Copyright: Sonikesh Kumar</span>
+    </footer>
+  );
+};
+
+export default Footer;
