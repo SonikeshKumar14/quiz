6b53b44a1fa6cc4f0282656041b3e11e3cb461d4
Sonikesh <91758635+SonikeshKumar14@users.noreply.github.com>
2023-09-06 00:24:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use((request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', process.env.FRONT_LINK);
+  response.setHeader('Access-Control-Allow-Origin', "http://localhost:3000");
   next();
 });
 app.use(require("./routes/quizzes"));
